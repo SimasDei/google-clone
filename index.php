ca2c076b2c0ea9162cc45c091ba4a80333607c25
SimasDei <simondeiman@gmail.com>
2019-03-09 09:38:19 +0200

    project setup

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <title>Google Clone</title>
+</head>
+<body>
+  <section class="main-section">
+  <div class="container">
+    <div class="logo-container">
+      <img src="./assets/logo.png" alt="logo" >
+    </div>
+    <div class="search-container">
+    <form action="./search.php" method="GET">
+      <input type="text" name="term" class="search-box">
+      <input type="submit" value="Search" class="search-button">
+    </form>
+    </div>
+    </div>
+  </section>
+</body>
+</html>
