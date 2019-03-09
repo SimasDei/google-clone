dbe9b2f026281b6a9e3acdac289f9de4a10f746b
SimasDei <simondeiman@gmail.com>
2019-03-09 11:24:26 +0200

    add main index markup and styling

--- index.php
+++ index.php
@@ -5,22 +5,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="./assets/css/style.css">
   <title>Google Clone</title>
 </head>
 <body>
-  <section class="main-section">
-  <div class="container">
-    <div class="logo-container">
-      <img src="./assets/logo.png" alt="logo" >
-    </div>
-    <div class="search-container">
-    <form action="./search.php" method="GET">
-      <input type="text" name="term" class="search-box">
-      <input type="submit" value="Search" class="search-button">
-    </form>
-    </div>
-    </div>
-  </section>
+  <div class="wrapper index-page">
+    <section class="main-section">
+      <div class="logo-container">
+        <img src="./assets/logo2.png" alt="logo" >
+      </div>
+      <div class="search-container">
+      <form action="./search.php" method="GET">
+        <input type="text" name="term" class="search-box">
+        <input type="submit" value="Search" class="search-button btn btn-primary">
+      </form>
+      </div>
+    </section>
+  </div>
 </body>
 </html>
