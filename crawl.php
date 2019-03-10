a479e1ce33afc004071179de98be822fcd1b6073
SimasDei <simondeiman@gmail.com>
2019-03-10 13:17:16 +0200

    setup webcrawler

--- crawl.php
+++ crawl.php
@@ -1,8 +1,16 @@
 <?php
 include './classes/DomDocumentParser.php';
 function followLinks($url) {
-  $parser = new DomDocumentParser ($url);
+  $parser = new DomDocumentParser($url);
+  $linkList = $parser->getLinks();
+
+  foreach ($linkList as $link) {
+    $href = $link->getAttribute('href');
+    echo $href;
+  }
 }
 
 $startUrl = "http://www.simas-dei.com";
 followLinks($startUrl);
+
+?>
